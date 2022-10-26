5865b6238944199ede3d457c3416209aa554379c
chellick <matsupus@gmail.com>
2022-10-26 20:35:40 +0300

    Update evolution code.py

--- evolution code.py
+++ evolution code.py
@@ -1,7 +1,9 @@
+from calendar import c
+from os import sep
 from random import randint
 import numpy as np
 import matplotlib.pyplot as plt
-
+child = []
 count = 0
 count2 = 0
 best = 0
@@ -11,35 +13,23 @@ parent2 = []
 population = []
 countc = 0
 count_str = []
+count_pr = 0
+indiv = int(input("сколько хотите индивидов?  "))
+dlin = int(input("длина индивидов  "))
+steps = int(input("кол-во поколений  "))
+sep_population = []
 
-
-#  code = int(input("сколько хотите популяций?  "))
-
-b = int(input("сколько хотите индивидов?  "))
-d = int(input("длина индивидов  "))
-#  while countc != d:
-while countc < d:
-    for i in range(b):
-        c1 = []
-        for i in range(d):
-            a = randint(0, 1)
-            c1.append(a)
-        population.append(c1)
-    #  print(population, "популяция")
-    #  print(space, c1, "\n", c2, "\n", c3, "\n", c4, "\n", c5, "\n", c6)
-    #  print(len(population))
-    #  if countc == d:
-    #    print(child)
-    #  else:
-    #    continue
-    tournament = randint(0, len(population) - 1)
-    #  print(tournament)
-    fp = population[tournament]
-    tournament = randint(0, len(population) - 1)
-    #  print(tournament)
-    sp = population[tournament]
-
-    #  print(fp, sp, "players")
+for i in range(indiv):
+    c1 = []
+    for i in range(dlin):
+        a = randint(0, 1)
+        c1.append(a)
+    population.append(c1)
+print(population, "первая")
+for i in range(steps):  #  Основной цикл создания популяций
+#  while countc < dlin:
+    fp = population[randint(0, len(population) - 1)]  #  Турнирный метод отбора родителей
+    sp = population[randint(0, len(population) - 1)]
 
     for i in fp:
         if i == 1:
@@ -47,75 +37,78 @@ while countc < d:
     for i in sp:
         if i == 1:
             count2 += 1
-            if count > count2:
-                parent1 = fp
-            elif count == count2:
-                parent1 = fp
-            else:
-                parent1 = sp
+    if count > count2:
+        parent1 = fp
+    elif count == count2:
+        parent1 = fp
+    else:
+        parent1 = sp
+    #  print(count, count2)
+    count = 0
+    count2 = 0
 
-    # print(parent1, "РОДИТЕЛЬ 1", "\n", "\n")
+    fp = population[randint(0, len(population) - 1)] #  выборка индивидов из массива population
+    sp = population[randint(0, len(population) - 1)]
 
-    tournament = randint(0, len(population) - 1)
-    fp = population[tournament]
-    #  print(tournament)
-    tournament = randint(0, len(population) - 1)
-    sp = population[tournament]
-    # print(fp, sp, "\n")
-    #  print(tournament)
     for i in fp:
         if i == 1:
             count += 1
     for i in sp:
         if i == 1:
             count2 += 1
-            if count > count2:
-                parent2 = fp
-            elif count == count2:
-                parent2 = fp
-            else:
-                parent2 = sp
-    # print(fp, sp, "players2")
-    # print(parent2, "РОДИТЕЛЬ 2", "\n", "\n", "\n")
+    if count > count2:
+        parent2 = fp
+    elif count == count2:
+        parent2 = fp
+    else:
+        parent2 = sp
+    count = 0
+    count2 = 0
 
-    rand = randint(0, d - 1)
+    rand = randint(0, dlin - 1)  # Метод выборки ребенка путем скрещивания частей генотипа родителей
     fh = parent1[:rand]  # first half
     sh = parent2[rand::]  # second half
     child = fh + sh
 
-    #   print(rand, "рандомное число из fh, sh списка", "\n")
-    #   print(space, parent1, "родитель 1", "\n", parent2, "родитель 2", "\n", "\n")
-    #   print(space, fh, "генотип 1", "\n", sh, "генотип 2", "\n", "\n")
-    #  print(child, "ребёнок")
-
-    for i in range(len(child)):
+    for i in range(len(child)):  #  Метод мутации
         rand = randint(1, 100)
         if rand == 1:
             child[[i] == 0] = 1
             child[[i] == 1] = 0  # метод замена элементов на основе одного условия
-    #  print(rand,"число", "\n")
-    #  print(child, "ребенок мутант")
-
-    
-
-    for i in child:
+    """
+    for i in child:  #  Нахождение количества едениц в генотипе ребёнка
         if i == 1:
             countc += 1
-            count_str.append(countc)#  print(countc) 
+    count_str.append(countc)
+    countc = 0
 
-    if countc < d:
-        countc = 0
     #  print(countc, d)
-    population.append(child)
     
+    #  print(child, "child")
+    """
+    sep_population.append(child)  # Добавление ребенка в список
+    #  print(sep_population, "changed population")
+    print(population, " популяция ", sep_population, " дочерняя популяция ")
+    
+    if len(population) == len(sep_population):
+        for i in population:
+            count_pr += sum(i)
+            count_str.append(count_pr)
+        for i in range(len(population)):
+            del population[0]
+        population = sep_population.copy()
+        for i in range(len(population)):
+            del sep_population[0]
 
-    del population[:b] 
+    count_pr = 0
+    #  print(population, "популяция")
+    
 
 for ex in range(len(count_str) + 1):
     count_str_x = list(range(1, ex + 1))
 #  print(count_str_x, "len str x")
-#  print(population)
-print(count_str, "len str")
+#  
+print(count_str, "пригодность")
 print(child)
 
 
